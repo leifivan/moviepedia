296f7c9e9f8120d7642f93d4b44f5ec7e078fadc
Leif Ivan Castro <leafvans@gmail.com>
2019-07-05 17:02:24 +0800

    added functionality

--- Rakefile
+++ Rakefile
@@ -12,4 +12,7 @@ Motion::Project::App.setup do |app|
   # Use `rake config' to see complete project settings.
   app.name = 'Moviepedia'
   app.api_version = "27"
+  app.main_activity = 'MainActivity'
+  app.sub_activities += %w(WikipediaActivity)
+  app.permissions = 'android.permission.INTERNET'
 end
---
+++ app/movies.rb
@@ -0,0 +1,34 @@
+class Movies
+
+  def self.names
+    ##Moviews name list
+    %w(
+        Shrek
+        Frozen
+        Toy\Story
+        FInding\Nemo
+        Despicable\Me
+        Inside\Out
+        Wall-E
+        Zootopia
+        Brave
+        Moana
+        Tangled
+        Coco
+        Ice\Age
+        Aladdin
+        Rango
+        Cars
+        Finding\Dory
+        Coraline
+        Ratatouille
+        The\Incredible
+        Monsters,\Inc.
+        Kung\Fu\Panda
+        Up
+        Wolf\Children
+        Ponyo
+       )
+  end
+
+end
